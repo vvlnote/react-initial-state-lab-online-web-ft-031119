7fb4e3db8523fb6bce2d9cb5f3335f1dfbf7aaa9
Joy Lin <vvlnote@hotmail.com>
2019-07-28 23:59:57 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,6 @@
-// your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+
+  render(){};
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,6 @@
-// your ImageSlider code here!
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+
+  render(){};
+}
